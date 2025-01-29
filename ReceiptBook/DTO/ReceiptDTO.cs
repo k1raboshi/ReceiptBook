d5a5c8c57bc1f4efa79db4411a740d3046cf76e0
Ivan Bondarenko <vanyabondarenko3189@gmail.com>
2025-01-30 01:02:54 +0200

    Add DTO

---
+++ ReceiptBook/DTO/ReceiptDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptBook.DTO
+{
+	public class CreateReceiptDTO
+	{
+		public string ReceiptName { get; set; }
+		public string ReceiptDescription { get; set; }
+		public string ReceiptInstructions { get; set; }
+
+		public CreateReceiptDTO(string receiptName, string receiptDescription, string receiptInstructions) 
+		{
+			ReceiptName = receiptName;
+			ReceiptDescription = receiptDescription;
+			ReceiptInstructions = receiptInstructions;
+		}
+	}
+}
